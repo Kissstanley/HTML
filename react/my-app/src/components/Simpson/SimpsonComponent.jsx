806bba11b23b00bb4e36679cb3715631ba653f70
Kissstanley <swyatoslawfyk@gmail.com>
2023-07-10 02:08:57 +0300

    complete third task at hw2

--- react/my-app/src/components/Simpson/SimpsonComponent.jsx
+++ react/my-app/src/components/Simpson/SimpsonComponent.jsx
@@ -4,7 +4,7 @@ const SimpsonComponent =(props)=>{
     return(
         <div className={styles.block}>
             <h1>{firstname}</h1>
-            <h2>{lastname}</h2>
+            <h1>{lastname}</h1>
             <h3>{age}</h3>
             <h3>{gender}</h3>
             <div className={styles.photo}>
